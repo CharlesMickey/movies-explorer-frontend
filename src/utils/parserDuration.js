cd2f7cdfd3b299afbbee505891c70472ec14d797
CharlesMickey <charlesmickey.ru@yandex.ru>
2021-09-01 13:03:27 +0300

    Feat: add parserDuration

--- src/utils/parserDuration.js
+++ src/utils/parserDuration.js
@@ -2,10 +2,8 @@ function parserDuration(duration) {
   const hours = Math.floor(duration / 60);
   const minutes = duration % 60;
 
-  if (duration % 60) {
-    const time = `${hours}ч ${minutes}м`;
-    return time;
-  }
+  const time = `${hours}ч ${minutes}м`;
+  return time;
 }
 
 export default parserDuration;
